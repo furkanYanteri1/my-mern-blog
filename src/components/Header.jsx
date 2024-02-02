072366338f943f1aba0aa447377ad2f973c43c76
Furkan Yanteri <furkan.yanteri@pitcher.com>
2024-02-02 22:19:11 +0300

    feat: header created

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import { Navbar } from 'flowbite-react'
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Header() {
+  return (
+    <Navbar className='border-b-2'>
+
+    </Navbar>
+  )
+}
