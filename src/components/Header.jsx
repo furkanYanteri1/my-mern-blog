faa395942ebb49f1acbf1aab2a0dbeb9a64e7aad
Furkan Yanteri <furkan.yanteri@pitcher.com>
2024-02-03 19:39:18 +0300

    fix: title logo text conditional sizing more detailed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 export default function Header() {
   return (
     <Navbar className='border-b-2'>
-        <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
+        <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-lg lg:text-2xl font-semibold dark:text-white'>
             <span className='py-1 px-2 bg-gradient-to-r from-cyan-600 to-indigo-500 rounded-lg text-white'>Furkan&apos;s</span>
             Blog
         </Link>
