abd5e37db314c10e8b3d76b753fd8913c1267e12
Furkan Yanteri <furkanyanteri@gmail.com>
2024-06-04 02:24:14 +0300

    fix: proxy for signup

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
    plugins: [react()],
-   proxy: {
-      '/api': {
-         target: 'http://localhost:3000',
-         secure: false,
-      }
-   }
-})
+   server: {
+      proxy: {
+         '/api': {
+            target: 'http://localhost:3000',
+            changeOrigin: true,
+            secure: false,
+         },
+      },
+   },
+});
