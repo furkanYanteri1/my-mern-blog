38c5d51f0d4df0edb8d4a9dcdccbb89ad47b5a72
Furkan Yanteri <furkanyanteri@gmail.com>
2024-06-04 01:27:24 +0300

    feat: [12] added proxy for signup -U

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+   plugins: [react()],
+   proxy: {
+      '/api': {
+         target: 'http://localhost:3000',
+         secure: false,
+      }
+   }
 })
